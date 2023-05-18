e2482b805931dcfd2a138c6f147c7a545370f0d1
_Chr1ss <113093934+Chrisiik@users.noreply.github.com>
2023-05-18 21:00:42 +0200

    Přidána funkce na zavírání popupu
    Přidána funkce na zavření Popupu.

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 document.querySelector('.menu').addEventListener('click', () => {
     document.querySelector('.menu-polozky').classList.toggle('skryte');
   });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    // Get the elements
+    var popupOverlay = document.querySelector(".popup-overlay");
+    var subscribeButton = document.getElementById("subscribe_button");
+  
+    // Add event listener to the subscribe button
+    subscribeButton.addEventListener("click", function() {
+      // Hide the popup by adding a CSS class
+      popupOverlay.classList.add("hidden");
+    });
+  });
+  
