5c563389a499798a021e054f4293cd904da12c93
_Chr1ss <113093934+Chrisiik@users.noreply.github.com>
2023-05-25 15:16:28 +0200

    Přidány funkce pro Popup
    - přidána funkce xmark, která zavírá popup
    - přidána funkce, která kontroluje vyplnění polí a také správný formát emailu

--- index.js
+++ index.js
@@ -2,15 +2,39 @@ document.querySelector('.menu').addEventListener('click', () => {
     document.querySelector('.menu-polozky').classList.toggle('skryte');
   });
 
-  document.addEventListener("DOMContentLoaded", function() {
-    // Get the elements
-    var popupOverlay = document.querySelector(".popup-overlay");
-    var subscribeButton = document.getElementById("subscribe_button");
-  
-    // Add event listener to the subscribe button
-    subscribeButton.addEventListener("click", function() {
-      // Hide the popup by adding a CSS class
-      popupOverlay.classList.add("hidden");
-    });
-  });
-  
+document.addEventListener("DOMContentLoaded", function() {
+  var popupOverlay = document.querySelector(".popup-overlay");
+  var subscribeButton = document.getElementById("subscribe_button");
+
+  subscribeButton.addEventListener("click", function() {
+    var nameInput = document.querySelector("input[name=jmeno]");
+    var emailInput = document.querySelector("input[type=email]");
+    
+    var emailPattern = /^\S+@\S+\.\S+$/;
+
+    if (nameInput.value !== "" && emailInput.value !== "") {
+      if (emailPattern.test(emailInput.value)) {
+        popupOverlay.classList.add("hidden");
+        errorContainer.style.display = "none";
+      } else {
+        errorContainer.textContent = "Please enter a valid email address.";
+        errorContainer.style.display = "block";
+      }
+    } else {
+      errorContainer.textContent = "Please fill in all fields.";
+      errorContainer.style.display = "block";
+    }
+
+});
+});
+
+const closeButton = document.getElementById("xmark");
+const popupOverlay = document.getElementById("popup-overlay");
+
+closeButton.addEventListener("click", function() {
+  popupOverlay.classList.add("hidden");
+  setTimeout(function() {
+    popupOverlay.classList.add("closed");
+  }, 1000);
+});
+
